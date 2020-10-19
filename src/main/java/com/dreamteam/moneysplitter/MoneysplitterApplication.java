27df7e615ded811dd8697ce7103f4d2ac9d054e7
Ultimatum <egrsar2000@gmail.com>
2020-10-19 21:56:53 +0400

    First Version

---
+++ src/main/java/com/dreamteam/moneysplitter/MoneysplitterApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.moneysplitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneysplitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneysplitterApplication.class, args);
+    }
+
+}
